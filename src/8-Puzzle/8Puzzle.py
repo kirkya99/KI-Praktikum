b7418c3023209ab707d16ae2c24b9cbfb6f11d22
fs-213804 <fs-213804@rwu.de>
2023-11-13 15:32:05 +0100

    Added 8-Puzzle and breadth-first search

---
+++ src/8-Puzzle/8Puzzle.py
@@ -0,0 +1,5 @@
+from Breitensuche import breadth_search
+
+if __name__ == '__main__':
+    nodes = []
+    breadth_search(nodes)
--- src/8-Puzzle/Breitensuche.py
+++ src/8-Puzzle/Breitensuche.py
@@ -1,6 +1,6 @@
-Nodes = []
-Init_State = [[1,2,3],[4,5,6],[7,8,0]]
+Init_State = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
 Final_State = [[1, 2, 3], [4, 5, 0], [6, 7, 8]]
 
 
-
+def breadth_search(node_list):
+    print("a")
