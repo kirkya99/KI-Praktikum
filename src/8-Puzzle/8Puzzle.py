48ff2c350fe100cca46f84e70465f307c5c828f9
fs-213804 <fs-213804@rwu.de>
2023-11-14 18:21:57 +0100

    updated bfs

--- src/8-Puzzle/8Puzzle.py
+++ src/8-Puzzle/8Puzzle.py
@@ -1,4 +1,5 @@
 from BreadthFirstSearch import breadth_search
+from Node import Node
 
 #Init_State = [[1, 2, 3], [4, 5, 0], [6, 7, 8]]
 Final_State = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
@@ -6,10 +7,13 @@ Init_State = [[1, 2, 3], [4, 5, 6], [7, 0, 8]]
 
 
 if __name__ == '__main__':
-    nodes = [Init_State]
+    node = Node(Init_State)
+    nodes = [node]
     res = breadth_search(nodes, Final_State)
+    
     print("Breitensuche:")
-    print(res[0])
+    if res[1] is None:
+        print(res[0])
     if res[1] is not None:
-        print(res[1])
+        print(res[0],": ",res[1])
 
