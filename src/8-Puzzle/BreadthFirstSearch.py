744dae517a87b9a6c82ea47876256209640f8ae9
fs-213804 <fs-213804@rwu.de>
2023-11-14 17:41:31 +0100

    implemented child node creation

--- src/8-Puzzle/BreadthFirstSearch.py
+++ src/8-Puzzle/BreadthFirstSearch.py
@@ -16,8 +16,100 @@ def target_reached(node, target):
 
 def descendants(node):
     child_nodes = []
-    print(node[0][0], node[0][1], node[0][2])
-    return child_nodes
+    new_node = []
+
+    if node[0][0] == 0:
+        new_node[0][0] = node[0][1]
+        new_node[0][1] = node[0][0]
+        child_nodes.append(new_node)
+        new_node[0][0] = node[1][0]
+        new_node[1][0] = node[0][0]
+        child_nodes.append(new_node)
+        return child_nodes
+    elif node[0][1] == 0:
+        new_node[0][1] = node[0][0]
+        new_node[0][0] = node[0][1]
+        child_nodes.append(new_node)
+        new_node[0][1] = node[0][2]
+        new_node[0][2] = node[0][1]
+        child_nodes.append(new_node)
+        new_node[0][1] = node[1][1]
+        new_node[1][1] = node[0][1]
+        child_nodes.append(new_node)
+        return child_nodes
+    elif node[0][2] == 0:
+        new_node[0][2] = node[0][1]
+        new_node[0][1] = node[0][2]
+        child_nodes.append(new_node)
+        new_node[0][2] = node[0][1]
+        new_node[1][2] = node[0][2]
+        child_nodes.append(new_node)
+        return child_nodes
+    elif node[1][0] == 0:
+        new_node[1][0] = node[0][0]
+        new_node[0][0] = node[1][0]
+        child_nodes.append(new_node)
+        new_node[1][0] = node[1][1]
+        new_node[1][1] = node[1][0]
+        child_nodes.append(new_node)
+        new_node[1][0] = node[2][0]
+        new_node[2][0] = node[1][0]
+        child_nodes.append(new_node)
+        return  child_nodes
+    elif node[1][1] == 0:
+        new_node[1][1] = node[0][1]
+        new_node[0][1] = node[1][1]
+        child_nodes.append(new_node)
+        new_node[1][1] = node[1][0]
+        new_node[1][0] = node[1][1]
+        child_nodes.append(new_node)
+        new_node[1][1] = node[1][2]
+        new_node[1][2] = node[1][1]
+        child_nodes.append(new_node)
+        new_node[1][1] = node[2][1]
+        new_node[2][1] = node[1][1]
+        child_nodes.append(new_node)
+        return child_nodes
+    elif node[1][2] == 0:
+        new_node[1][2] = node[0][2]
+        new_node[0][2] = node[1][2]
+        child_nodes.append(new_node)
+        new_node[1][2] = node[1][1]
+        new_node[1][1] = node[1][2]
+        child_nodes.append(new_node)
+        new_node[1][2] = node[2][2]
+        new_node[2][2] = node[1][1]
+        child_nodes.append(new_node)
+        return child_nodes
+    elif node[2][0] == 0:
+        new_node[2][0] = node[1][0]
+        new_node[1][0] = node[2][0]
+        child_nodes.append(new_node)
+        new_node[2][0] = node[2][1]
+        new_node[2][1] = node[2][0]
+        child_nodes.append(new_node)
+        return child_nodes
+    elif node[2][1] == 0:
+        new_node[2][1] = node[2][0]
+        new_node[2][0] = node[2][1]
+        child_nodes.append(new_node)
+        new_node[2][1] = node[1][1]
+        new_node[1][1] = node[2][1]
+        child_nodes.append(new_node)
+        new_node[2][1] = node[2][2]
+        new_node[2][2] = node[2][1]
+        child_nodes.append(new_node)
+        return child_nodes
+    elif node[2][2] == 0:
+        new_node[2][2] = node[1][2]
+        new_node[1][2] = node[2][2]
+        child_nodes.append(new_node)
+        new_node[2][2] = node[2][1]
+        new_node[2][1] = node[2][2]
+        child_nodes.append(new_node)
+        return child_nodes
+    else:
+        return []
 
 
 def append(new_nodes, child_nodes):
